74a345ea53d6675b2f32326b49396c96ffbe8fcd
Julia Peres <147351970+jpc-gamer@users.noreply.github.com>
2025-04-02 14:06:16 -0300

    Add files via upload

--- Variáveis e Estrutura de Condição/ex06.php
+++ Variáveis e Estrutura de Condição/ex06.php
@@ -1,12 +1,39 @@
-<?php
-	$distancia_total = 100;
-    $consumo_medio = 5;
-    $valor_gasolina = 6;
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora de Idade em Dias</title>
+</head>
+<body>
+    <h1>Calcular o custo da viagem</h1>
+    <h3> Informe as seguintes informações do seu carro:</h3>
 
-    $media = $distancia_total / $consumo_medio;
-     $valor_final = $media * $valor_gasolina;
+    <form method="post">
+        <label for="km/l">Km/L:</label>
+        <input type="number" name="km/l" id="km/l" required><br><br>
 
-    echo "<p> O custo da sua viagem é R$: ", $valor_final, "</p>";
+        <label for="distancia">Distância Percorrida: </label>
+        <input type="number" name="distancia" id="distancia" required><br><br>
 
+        <label for="preco">Preço Unitário do Combustível:</label>
+        <input type="number"  step="0.01" name="preco" id="preco" required><br><br>
 
-?>
+        <input type="submit" value="Calcular">
+    </form>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $Km_L = $_POST['km/l'];
+        $Distancia_Percorrida = $_POST['distancia'];
+        $Preco_Combustivel = $_POST['preco'];
+
+        $custo_total = $Distancia_Percorrida/ $Km_L * $Preco_Combustivel;
+
+        echo "<h2>O custo da sua viagem é R$: $custo_total </h2>";
+    }
+    ?>
+
+</body>
+</html
